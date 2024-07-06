3f31d86c9e3ae9747c37229c81232b2ee76e74b6
aminul157246 <aminul.islam.157246@gmail.com>
2024-07-06 20:27:56 +0600

    file and directories

---
+++ files_and_directories/directory.py
@@ -0,0 +1,10 @@
+from pathlib import Path
+
+# path = Path("email")
+# print(path.mkdir())
+# print(path.rmdir())
+
+path = Path()
+for file in path.glob('*.py'):
+    print(file)    #directory.py
+                   # ecomerce.py
